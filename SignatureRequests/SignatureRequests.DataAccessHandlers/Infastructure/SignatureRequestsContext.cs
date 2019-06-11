23e6f6c07b5fd32eabaa9531e7a32dee5dce5469
Iris Top <imtop@iastate.edu>
2019-06-11 15:49:11 -0500

    Removed extra files/lines

--- SignatureRequests/SignatureRequests.DataAccessHandlers/Infastructure/SignatureRequestsContext.cs
+++ SignatureRequests/SignatureRequests.DataAccessHandlers/Infastructure/SignatureRequestsContext.cs
@@ -13,22 +13,9 @@ namespace SignatureRequestsContext.DataAccessHandlers
         {
 
         }
-        //public DbSet<PainPointEntity> PainPoints { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            //modelBuilder.Entity<PainPointTypeEntity>()
-            //    .HasOne<PainPointEntity>(sc => sc.PainPoint)
-            //    .WithMany(s => s.TypeEntities)
-            //    .HasForeignKey(sa => sa.PainPointId);
 
-            //modelBuilder.Entity<PainPointTypeEntity>()
-            //    .HasOne<TypeEntity>(sa => sa.Type)
-            //    .WithMany(a => a.TypeEntities)
-            //    .HasForeignKey(sa => sa.TypeId);
-            //foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
-            //{
-            //    relationship.DeleteBehavior = DeleteBehavior.Restrict;
-            //}
         }
     }
 }
