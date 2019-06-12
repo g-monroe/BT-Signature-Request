81d5aacfb1ef910e3c0011ad1182cf5c9684445b
gavinmonroe <gavin.monroe@buildertrend.com>
2019-06-12 10:38:01 -0500

    Last time

--- SignatureRequests/SignatureRequests.Core/Items/SignatureItem.cs
+++ SignatureRequests/SignatureRequests.Core/Items/SignatureItem.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SignatureRequests.Core.Entities
+namespace SignatureRequests.Core.Items
 {
     public class SignatureItem
     {
