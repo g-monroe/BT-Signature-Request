2e42782c28bf105db9990fe55820e13bf2e862ed
gavinmonroe <gavin.monroe@buildertrend.com>
2019-06-18 08:16:56 -0500

    Box API, Done with all the handlers, managers, and controllers.

---
+++ SignatureRequests/SignatureRequests.Core/Interfaces/DataAccessHandlers/IBoxHandler.cs
@@ -0,0 +1,12 @@
+﻿using SignatureRequests.Core.Entities;
+using SignatureRequests.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SignatureRequests.Core.Interfaces.DataAccessHandlers
+{
+    public interface IBoxHandler : IBaseHandler<BoxEntity>
+    {
+        IEnumerable<BoxEntity> GetAllInclude();
+    }
+}
---
+++ SignatureRequests/SignatureRequests.Core/ResponseObjects/BoxResponse.cs
@@ -0,0 +1,25 @@
+﻿using SignatureRequests.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignatureRequests.Core.ResponseObjects
+{
+    public class BoxResponse
+    {
+        public int Id { get; set; }
+        public int? Width { get; set; }
+        public int? Length { get; set; }
+        public int? X { get; set; }
+        public int? Y { get; set; }
+        public string Type { get; set; }
+        public string SignerType { get; set; }
+        public string SignedStatus { get; set; }
+        public RequestEntity Request { get; set; }
+        public int RequestId { get; set; }
+        public SignatureEntity Signature { get; set; }
+        public int? SignatureId { get; set; }
+    }
+}
