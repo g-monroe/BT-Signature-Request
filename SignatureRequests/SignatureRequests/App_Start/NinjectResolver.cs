183b7e304cbad1d091d3b8ab740aac9a2393ea2e
gavinmonroe <gavin.monroe@buildertrend.com>
2019-06-18 08:16:56 -0500

    Box API, Done with all the handlers, managers, and controllers.

--- SignatureRequests/SignatureRequests/App_Start/NinjectResolver.cs
+++ SignatureRequests/SignatureRequests/App_Start/NinjectResolver.cs
@@ -37,7 +37,8 @@ namespace SignatureRequests.App_Start
             kernel.Bind<ISignatureLibManager>().To<SignatureLibManager>().InSingletonScope();
             kernel.Bind<ISignatureManager>().To<SignatureManager>().InSingletonScope();
             kernel.Bind<ISignatureHandler>().To<SignatureHandler>().InSingletonScope();
-
+            kernel.Bind<IBoxManager>().To<BoxManager>().InSingletonScope();
+            kernel.Bind<IBoxHandler>().To<BoxHandler>().InSingletonScope();
             return kernel;
         }
 
