ded2ef86c7d5fb54ce22ea230c3eaafe223add72
Iris Top <imtop@iastate.edu>
2019-06-26 08:59:18 -0500

    Signature page addded

---
+++ SignatureRequests/SignatureRequests.Webs/Scripts/react/src/Components/Signatures/ImageViewer.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import test from '../../TestImages/testMadison.jpeg'
+
+export interface IImageViewerProps {
+    title?:string
+}
+ 
+export interface IImageViewerState {
+    
+}
+ 
+class ImageViewer extends React.Component<IImageViewerProps, IImageViewerState> {
+    state = {   }
+    render() { 
+        return ( 
+            <div id = 'imageView'>
+                <h1>{this.props.title || ''}</h1>
+                <img src = {test} width = {'50%'} height = {'50%'}></img>
+            </div>
+         );
+    }
+}
+ 
+export default ImageViewer;
---
+++ SignatureRequests/SignatureRequests.Webs/Scripts/react/src/Components/Signatures/SignaturePage.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import SignatureBox from './SignatureBox';
+import ImageViewer from './ImageViewer';
+
+export interface ISignaturePageProps {
+    
+}
+ 
+export interface ISignaturePageState {
+    
+}
+ 
+class SignaturePage extends React.Component<ISignaturePageProps, ISignaturePageState> {
+    state = {   }
+    render() { 
+        return ( 
+            <div id = 'signaturePage'>
+            <div id = 'imageContainer'>
+                <ImageViewer title = "Current Signature"></ImageViewer>
+                <ImageViewer title = "Current Initials"></ImageViewer>
+            </div>
+                <SignatureBox></SignatureBox>
+            </div>
+         );
+    }
+}
+ 
+export default SignaturePage;
