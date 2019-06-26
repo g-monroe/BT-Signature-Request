d8dc20aa8ed266476b99d57e285ccce43afc2cf3
Iris Top <imtop@iastate.edu>
2019-06-26 09:04:43 -0500

    extra enums added

---
+++ SignatureRequests/SignatureRequests.Webs/Scripts/react/src/Util/Enums/SelectTypes.ts
@@ -0,0 +1,18 @@
+
+
+export const inputMethods : string[] = [
+    'Draw',
+    'Type',
+]
+
+export const inputTypes : string [] = [
+    'Signature',
+    'Initial',
+    'Date'
+]
+
+export const manualInputTypes : string [] = [
+    'Signature',
+    'Initial',
+
+]
