ccfbaa9f2db930dbd6068a4fab90f93e1ca03cf2
gavinmonroe <gavin.monroe@buildertrend.com>
2019-06-27 15:09:40 -0500

    Just to keep it updated. And then fixes coming next.

---
+++ SignatureRequests/SignatureRequests.Core/Entities/GroupEntity.cs
@@ -0,0 +1,24 @@
+﻿using BTSuggestions.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignatureRequests.Core.Entities
+{
+    public class GroupEntity: BaseEntity
+    {
+        [Required]
+        public virtual RequestEntity Request { get; set; }
+
+        public int RequestId { get; set; }
+
+        public virtual FormEntity FormEntity { get; set; }
+
+        public int FormId { get; set; }
+
+        
+    }
+}
