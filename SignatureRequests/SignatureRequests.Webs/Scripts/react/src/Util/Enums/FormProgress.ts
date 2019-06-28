5e4aa325b9286a0e2c7a1b213aba5400be6d6f78
maxminard <maxwell.minard@buildertrend.com>
2019-06-28 11:23:45 -0500

    Fixed enum for code review

--- SignatureRequests/SignatureRequests.Webs/Scripts/react/src/Util/Enums/FormProgress.ts
+++ SignatureRequests/SignatureRequests.Webs/Scripts/react/src/Util/Enums/FormProgress.ts
@@ -1,16 +1,7 @@
 enum FormProgress {
-    PENDING,
-    COMPLETED,
-    REJECTED
-}
-
-export const changeToValue = (value: number) : string => {
-    switch(value){
-        case 0: return "pending";
-        case 1: return "completed";
-        case 2: return "rejected";
-    }
-    return "unknown";
+    PENDING = "pending",
+    COMPLETED ="completed",
+    REJECTED ="rejected"
 }
 
 export default FormProgress;
