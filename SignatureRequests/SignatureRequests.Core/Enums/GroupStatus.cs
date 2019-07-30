95f9fea01e0423e30fd4b17240b231e832acd4f9
Iris Top <imtop@iastate.edu>
2019-07-30 13:34:30 -0500

    Enums added

---
+++ SignatureRequests/SignatureRequests.Core/Enums/GroupStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignatureRequests.Core.Enums
+{
+    public class GroupStatusEnum
+    {
+        public static string PENDING = "pending";
+        public static string COMPLETE = "Complete";
+        public static string NOTSTARTED = "Not Started";
+        
+    }
+}
